726bd2cc2f2f8751778716f9ba371c40f90ad0ce
unknown <hamzahmitou@gmail.com>
2019-09-23 12:30:57 +0300

    Testing event listener

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
-var inputGroup = Array.from(document.querySelectorAll(".input-group i, .error-msg"));
-var fname =document.getElementsByName("fname");
+const fname = document.getElementsByClassName("firstName");
+const lname = document.getElementsByClassName("lastName");
+const email = document.getElementsByClassName("email");
+const pswd = document.getElementsByClassName("password");
+const form = document.getElementById("form");
 
-inputGroup.map((item, index) => {
-    item.style.display = "none";
+
+/*Looks like it's not an email*/
+
+form.addEventListener("submit", function (event){
+    if(fname[0].children[0].value === "" && lname[0].children[0].value === "" && email[0].children[0].value === "" && pswd[0].children[0].value === "") {
+        fname[0].children[1].classList.add("show");
+        fname[0].children[2].classList.add("show");
+        lname[0].children[1].classList.add("show");
+        lname[0].children[2].classList.add("show");
+        email[0].children[1].classList.add("show");
+        email[0].children[2].classList.add("show");
+        pswd[0].children[1].classList.add("show");
+        pswd[0].children[2].classList.add("show");
+        event.preventDefault();
+
+    }
+
+    /* else if(email[0].children[0].value && (/(^\w.*@\w+\.\w)/).test(email[0].children[0].value)) {
+        event.preventDefault();
+        console.log("email invalid");
+    } */
+    
 });
+
+
